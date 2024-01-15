abcaa0c593c3e6d86b74c07a3b5841f0006e0b2b
aknay <aknay@outlook.com>
2024-01-15 23:34:10 +0800

    add draft stock filter

--- lib/view/stock/transactions/stock.transactions.screen.dart
+++ lib/view/stock/transactions/stock.transactions.screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:inventory_frontend/view/main/drawer.dart';
+import 'package:inventory_frontend/view/stock/transactions/filter.stock.transaction.screen.dart';
 import 'package:inventory_frontend/view/stock/transactions/stock.transaction.list.view.dart';
 
 class StockTransactionsScreen extends ConsumerWidget {
@@ -10,7 +12,20 @@ class StockTransactionsScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       drawer: const DrawerWidget(),
-      appBar: AppBar(title: const Text("Stock Transactions")),
+      appBar: AppBar(
+        title: const Text("Stock Transactions"),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FilterStockTransactionScreen()),
+                );
+                // await _submit(ref: ref);
+              },
+              icon: const FaIcon(FontAwesomeIcons.filter)),
+        ],
+      ),
       body: const StockTransactionListView(),
     );
   }
