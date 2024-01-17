55f7d33b6166870ff9d19e2d39d9ac5df895f195
aknay <aknay@outlook.com>
2024-01-17 23:03:35 +0800

    add expandable toogle button

--- lib/view/stock/transactions/filter.stock.transaction.screen.dart
+++ lib/view/stock/transactions/filter.stock.transaction.screen.dart
@@ -1,7 +1,11 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum StockFiltering { all, stockIn, stockOut, stockAdjust }
+
+final stockFilteringStateProvider = StateProvider<StockFiltering>(
+  (ref) => StockFiltering.all,
+);
 
 class FilterStockTransactionScreen extends ConsumerWidget {
   const FilterStockTransactionScreen({super.key});
@@ -15,10 +19,7 @@ class FilterStockTransactionScreen extends ConsumerWidget {
         actions: [
           TextButton(
             child: const Text('Clear'),
-            onPressed: () async {
-              // await _submit(ref: ref);
-            },
-            // icon: const FaIcon(FontAwesomeIcons.c)),
+            onPressed: () async {},
           )
         ],
       ),
@@ -27,7 +28,8 @@ class FilterStockTransactionScreen extends ConsumerWidget {
           Text("Stock"),
           Row(
             children: [
-              Expanded(child: StockTransactionToggleButton()),
+              // we need to use expanded for using LayoutBuilder
+              Expanded(child: FilterStockToggleButton()),
             ],
           ),
         ],
@@ -36,46 +38,59 @@ class FilterStockTransactionScreen extends ConsumerWidget {
   }
 }
 
-final _toggleButtonStateProvider = StateProvider<List<bool>>(
-  (ref) => [true, false, false, true],
-);
-
-class StockTransactionToggleButton extends ConsumerWidget {
-  const StockTransactionToggleButton({super.key});
+class FilterStockToggleButton extends ConsumerStatefulWidget {
+  const FilterStockToggleButton({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // return LayoutBuilder(builder: (context, constraints) {
-    final v = ref.watch(_toggleButtonStateProvider);
-    log("the value here is $v");
-    return ToggleButtons(
-      renderBorder: false,
-      // constraints: BoxConstraints.expand(width: constraints.maxWidth / 4),
-      borderRadius: BorderRadius.circular(5),
-      isSelected: v,
-      selectedColor: Colors.green,
-      onPressed: (index) {
-        final value = ref.read(_toggleButtonStateProvider);
+  ConsumerState<ConsumerStatefulWidget> createState() => _FilterStockToggleButtonState();
+}
 
-        for (int buttonIndex = 0; buttonIndex < value.length; buttonIndex++) {
-          if (buttonIndex == index) {
-            value[buttonIndex] = !value[buttonIndex];
-          } else {
-            value[buttonIndex] = false;
-          }
-        }
+class _FilterStockToggleButtonState extends ConsumerState<FilterStockToggleButton> {
+  final List<bool> _isSelected = [true, false, false, false];
 
-        // value[index] = !value[index];
-        log("the value is $value");
-        ref.read(_toggleButtonStateProvider.notifier).state = value;
-      },
-      children: const [
-        Text('All'),
-        Text('In'),
-        Text('Out'),
-        Text('Adjust'),
-      ],
-    );
-    // });
+  @override
+  Widget build(BuildContext context) {
+    //ref: https://github.com/invoiceninja/admin-portal/blob/54777d31bd42eab571e5b8c21100299a65500a41/lib/ui/app/forms/app_toggle_buttons.dart#L23
+    return LayoutBuilder(builder: (context, constraints) {
+      double toggleWidth = (constraints.maxWidth - 26) / 4;
+      final labels = ["All", "In", "Out", "Adjust"];
+
+      final children = labels
+          .map((label) => SizedBox(
+                width: toggleWidth,
+                child: Center(
+                    child: Text(
+                  label[0].toUpperCase() + label.substring(1),
+                  maxLines: 1,
+                  overflow: TextOverflow.clip,
+                )),
+              ))
+          .toList();
+
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ToggleButtons(
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          isSelected: _isSelected,
+          onPressed: (index) {
+            setState(() {
+              for (int i = 0; i < _isSelected.length; i++) {
+                _isSelected[i] = i == index;
+                if (index == 0) {
+                  ref.read(stockFilteringStateProvider.notifier).state = StockFiltering.all;
+                } else if (index == 1) {
+                  ref.read(stockFilteringStateProvider.notifier).state = StockFiltering.stockIn;
+                } else if (index == 2) {
+                  ref.read(stockFilteringStateProvider.notifier).state = StockFiltering.stockOut;
+                } else if (index == 3) {
+                  ref.read(stockFilteringStateProvider.notifier).state = StockFiltering.stockAdjust;
+                }
+              }
+            });
+          },
+          children: children,
+        ),
+      );
+    });
   }
 }
