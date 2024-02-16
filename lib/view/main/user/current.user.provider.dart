518d6b1b3d7af5c46d7abb047e6407aa727652f3
aknay <aknay@outlook.com>
2024-02-16 14:12:13 +0800

    fix loading of current user every time drawer is opened

--- lib/view/main/user/current.user.provider.dart
+++ lib/view/main/user/current.user.provider.dart
@@ -1,13 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:warelake/data/user/user.service.dart';
 import 'package:warelake/domain/user/valueobject.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'current.user.provider.g.dart';
 
-@riverpod
+@Riverpod(keepAlive: true)
 Future<User> currentUser(CurrentUserRef ref) async {
   final userService = ref.watch(userServiceProvider);
   final userOrError = await userService.getCurrentUser();
+  if (kDebugMode) {
+    await Future.delayed(const Duration(seconds: 1));
+  }
   if (userOrError.isLeft()) {
     throw Exception('Unable to get current user');
   }
