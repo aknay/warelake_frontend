f5db362c9049fbe8c757d184eb47bacacda062fb
aknay <aknay@outlook.com>
2024-02-27 13:08:06 +0800

    able to test api for adding images item variations

--- lib/domain/item/requests.dart
+++ lib/domain/item/requests.dart
@@ -22,3 +22,30 @@ class ItemImageRequest {
     };
   }
 }
+
+class ItemVariationImageRequest {
+  String itemId;
+  String itemVariationId;
+  String teamId;
+
+  File imagePath;
+  String? imageId;
+  String? imageName;
+
+  ItemVariationImageRequest({
+    required this.itemId,
+    required this.teamId,
+    required this.imagePath,
+    required this.itemVariationId,
+    this.imageId,
+    this.imageName,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'item_id': itemId,
+      'item_variation_id': itemVariationId,
+      'image_name': imageName,
+    };
+  }
+}
