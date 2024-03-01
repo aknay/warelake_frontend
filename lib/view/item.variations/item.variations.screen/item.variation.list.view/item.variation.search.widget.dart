343772a4cbf42e64ef571c598442b000b6835121
aknay <aknay@outlook.com>
2024-03-01 19:26:23 +0800

    able to scan and search by barcode

--- lib/view/item.variations/item.variations.screen/item.variation.list.view/item.variation.search.widget.dart
+++ lib/view/item.variations/item.variations.screen/item.variation.list.view/item.variation.search.widget.dart
@@ -1,9 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:warelake/view/barcode/barcode.scanner.value.controller.dart';
+import 'package:warelake/view/barcode/barcode.scanner.widget.dart';
 
-final searchItemVariationByBarcodeProvider = StateProvider<Option<String>>(
-  (ref) => const None(),
+final searchItemVariationByBarcodeProvider = StateProvider.autoDispose<Option<String>>(
+  (ref) {
+    return ref.watch(barcodeScannerValueControllerProvider);
+  },
 );
 
 class ItemVariationSearchWidget extends ConsumerWidget {
@@ -11,21 +15,29 @@ class ItemVariationSearchWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return TextFormField(
-      initialValue: ref.watch(searchItemVariationByBarcodeProvider).toNullable(),
-      onChanged: (value) async {
-        if (value.isEmpty) {
-          ref.read(searchItemVariationByBarcodeProvider.notifier).state = const None();
-        } else if (value.length > 2) {
-          ref.read(searchItemVariationByBarcodeProvider.notifier).state = Some(value);
-        }
-      },
-      decoration: InputDecoration(
-        prefixIcon: const Icon(Icons.search, color: Colors.white),
-        hintText: "Search item by barcode",
-        labelStyle: Theme.of(context).textTheme.bodyLarge,
-        border: const OutlineInputBorder(),
-      ),
+    return Row(
+      children: [
+        Expanded(
+          child: TextFormField(
+            key: UniqueKey(),
+            initialValue: ref.watch(searchItemVariationByBarcodeProvider).toNullable(),
+            onChanged: (value) async {
+              if (value.isEmpty) {
+                ref.read(searchItemVariationByBarcodeProvider.notifier).state = const None();
+              } else if (value.length > 2) {
+                ref.read(searchItemVariationByBarcodeProvider.notifier).state = Some(value);
+              }
+            },
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.search, color: Colors.white),
+              hintText: "Search item by barcode",
+              labelStyle: Theme.of(context).textTheme.bodyLarge,
+              border: const OutlineInputBorder(),
+            ),
+          ),
+        ),
+        const BarcodeScannerWidget()
+      ],
     );
   }
 }
