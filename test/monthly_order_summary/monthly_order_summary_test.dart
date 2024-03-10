fc46d9d49bfc82331ee40d0a5c3b7ab5c26dfc1c
aknay <aknay@outlook.com>
2024-03-10 21:54:41 +0800

    able to add po summary to monthly summar

--- test/monthly_order_summary/monthly_order_summary_test.dart
+++ test/monthly_order_summary/monthly_order_summary_test.dart
@@ -1,11 +1,16 @@
 import 'dart:convert';
 
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
+import 'package:warelake/data/bill.account/bill.account.repository.dart';
 import 'package:warelake/data/currency.code/valueobject.dart';
 import 'package:warelake/data/item/item.repository.dart';
 import 'package:warelake/data/monthly.order.summary/monthly.order.summary.repository.dart';
+import 'package:warelake/data/purchase.order/purchase.order.repository.dart';
 import 'package:warelake/data/team/team.repository.dart';
+import 'package:warelake/domain/item/entities.dart';
+import 'package:warelake/domain/purchase.order/entities.dart';
 import 'package:warelake/domain/team/entities.dart';
 
 import '../helpers/sign.in.response.dart';
@@ -15,9 +20,14 @@ void main() async {
   final teamApi = TeamRepository();
   final itemApi = ItemRepository();
   final monthlyOrderSummaryApi = MonthlyOrderSummaryRepository();
+  final billAccountApi = BillAccountRepository();
+  final purchaseOrderApi = PurchaseOrderRepository();
   late String firstUserAccessToken;
 
   late String teamId;
+  late String billAccountId;
+  late Item shirtItem;
+  late Item jeanItem;
 
   setUpAll(() async {
     final email = generateRandomEmail();
@@ -50,18 +60,61 @@ void main() async {
     final createdOrError = await teamApi.create(team: newTeam, token: firstUserAccessToken);
     expect(createdOrError.isRight(), true);
     teamId = createdOrError.toIterable().first.id!;
+
+    final accountListOrError = await billAccountApi.list(teamId: teamId, token: firstUserAccessToken);
+    expect(accountListOrError.isRight(), true);
+    final account = accountListOrError.toIterable().first.data.first;
+    billAccountId = account.id!;
     final shirt = getShirt();
     final jean = getJean();
 
     final shirtCreatedOrError = await itemApi.createItem(item: shirt, teamId: teamId, token: firstUserAccessToken);
-    shirtCreatedOrError.toIterable().first;
+    shirtItem = shirtCreatedOrError.toIterable().first;
 
     final jeansCreatedOrError = await itemApi.createItem(item: jean, teamId: teamId, token: firstUserAccessToken);
-    jeansCreatedOrError.toIterable().first;
+    jeanItem = jeansCreatedOrError.toIterable().first;
   });
 
   test('monthly order summary can be retrieved even it has not be created yet', () async {
     final monthlyOrderSummaryOrError = await monthlyOrderSummaryApi.get(teamId: teamId, token: firstUserAccessToken);
     expect(monthlyOrderSummaryOrError.isRight(), true);
+  });
+
+  test('monthly order summary will be increased for PO when a po is added', () async {
+    final po = PurchaseOrder.create(
+        accountId: billAccountId,
+        date: DateTime.now(),
+        currencyCode: CurrencyCode.AUD,
+        lineItems: getLineItems(items: [Tuple2(5, shirtItem), Tuple2(10, jeanItem)]),
+        subTotal: 10,
+        purchaseOrderNumber: "PO-0001",
+        total: 20);
+
+    final poCreatedOrError =
+        await purchaseOrderApi.issuedPurchaseOrder(purchaseOrder: po, teamId: teamId, token: firstUserAccessToken);
+      await Future.delayed(const Duration(seconds: 1));
+    expect(poCreatedOrError.isRight(), true);
+
+    {
+      final now = DateTime.now();
+      final po = poCreatedOrError.toIterable().first;
+      
+      await purchaseOrderApi.receivedItems(
+          purchaseOrderId: po.id!, date: now, teamId: teamId, token: firstUserAccessToken);
+
+      await Future.delayed(const Duration(seconds: 1));
+
+      await purchaseOrderApi.get(purchaseOrderId: po.id!, teamId: teamId, token: firstUserAccessToken);
+    }
+
+    final monthlyOrderSummaryOrError = await monthlyOrderSummaryApi.get(teamId: teamId, token: firstUserAccessToken);
+    expect(monthlyOrderSummaryOrError.isRight(), true);
+    final monthlyOrderSummary = monthlyOrderSummaryOrError.toIterable().first;
+    expect(monthlyOrderSummary.purchaseOrderCount, 1);
+    final poCreated = poCreatedOrError.toIterable().first;
+    final amount = poCreated.lineItems
+        .map((e) => e.rate * e.quantity)
+        .fold(0, (previousValue, element) => previousValue + element);
+    expect(monthlyOrderSummary.purchaseOrderAmount, amount / 1000);
   });
 }
