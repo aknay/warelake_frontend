c6d878442d059718ccd247d45a62acfa01b4740c
aknay <aknay@outlook.com>
2024-06-12 17:45:11 +0800

    fix ux for bill account list

--- lib/view/bill.account/bill.account.list.view.dart
+++ lib/view/bill.account/bill.account.list.view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:warelake/view/bill.account.selection/bill.account.controller.dart';
+import 'package:warelake/view/common.widgets/amount.text.dart';
 import 'package:warelake/view/items/item.list.controller.dart';
 import 'package:warelake/view/routing/app.router.dart';
 import 'package:warelake/view/utils/async_value_ui.dart';
@@ -23,16 +24,16 @@ class BillAccountListView extends ConsumerWidget {
           return ListView(
               children: data
                   .map((e) => ListTile(
-                        title: Text(e.name),
-                        subtitle: Text("${e.currencyCodeAsEnum.name} ${e.balance}"),
-                        onTap: () {
-                          context.goNamed(
-                            AppRoute.billAccount.name,
-                            pathParameters: {'id': e.id!},
-                          );
-                          // Navigator.pop(context, e);
-                        },
-                      ))
+                      title: Text(e.name),
+                      subtitle: AmountText(e.balance, style: Theme.of(context).textTheme.bodyLarge),
+                      onTap: () {
+                        context.goNamed(
+                          AppRoute.billAccount.name,
+                          pathParameters: {'id': e.id!},
+                        );
+                        // Navigator.pop(context, e);
+                      },
+                      trailing: const Icon(Icons.arrow_forward_ios)))
                   .toList());
         },
         error: (Object error, StackTrace stackTrace) => Text('Error: $error'),
